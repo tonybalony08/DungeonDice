62a26054b1f078dab6c124c1582b76b629359822
tonybalony08 <anthonycalixto08@gmail.com>
2026-02-06 10:50:14 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by CALIXTO, ANTHONY on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DungeonDiceTests/DungeonDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceTests.swift
+//  DungeonDiceTests
+//
+//  Created by CALIXTO, ANTHONY on 2/6/26.
+//
+
+import Testing
+@testable import DungeonDice
+
+struct DungeonDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
